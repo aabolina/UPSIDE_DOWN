d5680f1a023f277765aa54f6804cecd34248b3c7
Evelīna Āboliņa <47450419+aabolina@users.noreply.github.com>
2019-05-10 18:57:07 +0300

    Add files via upload

---
+++ Assets/IdleToCrouch1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleToCrouch1 : MonoBehaviour
+{
+    public Animator animator;
+    float InputX;
+    public float InputY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the animator
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputY = Input.GetAxis("Vertical");
+        InputX = Input.GetAxis("Horizontal");
+        animator.SetFloat("InputY", InputY);
+        animator.SetFloat("InputX", InputX);
+    }
+}
